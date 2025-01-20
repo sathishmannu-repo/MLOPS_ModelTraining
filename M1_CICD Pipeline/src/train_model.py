0307903750c1ef98077854a3b77a557a1a4ae9f5
bpallavi8820 <bpallavi8820@gmail.com>
2025-01-20 22:31:45 +0530

    Update train_model.py

--- M1_CICD Pipeline/src/train_model.py
+++ M1_CICD Pipeline/src/train_model.py
@@ -16,7 +16,9 @@ X = data.drop(columns=['PRICE'])
 y = data['PRICE']
 
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Initialize and train the model
 model = HousingModel()
