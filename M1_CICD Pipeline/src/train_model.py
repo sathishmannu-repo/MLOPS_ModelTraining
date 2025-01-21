3f51d8bc39a96593720432e966056416c86d4e0f
bpallavi8820 <bpallavi8820@gmail.com>
2025-01-21 14:49:32 +0530

    Update train_model.py

--- M1_CICD Pipeline/src/train_model.py
+++ M1_CICD Pipeline/src/train_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import math
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from housing_model import HousingModel  # Make sure the path is correct
